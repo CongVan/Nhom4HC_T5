2cc29533ba71b9d7664ab518ab258c0c4a23f686
hoangcp <hoangcp4@gmail.com>
2018-10-28 13:37:54 +0700

    Revert "Vấn đề - Thêm, Xem danh sách, Xóa"
    This reverts commit 545b7bfb15cc06909cd838e6d795ecb573ddc860.

--- Issue/Issue.Create/Areas/Issue/Controllers/IssueController.cs
+++ Issue/Issue.Create/Areas/Issue/Controllers/IssueController.cs
@@ -1,6 +1,4 @@
-﻿using Issue.Create.Areas.Issue.Models;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -13,42 +11,12 @@ namespace Issue.Create.Areas.Issue.Controllers
     {
         //
         // GET: /Issue/Issue/
-       
+        string cnnString = ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
+
         public ActionResult Create()
         {
-            if (Request.Form["VanDeID"] != null)
-                ViewBag.VanDeID = Request.Form["VanDeID"].ToString();
-            else
-                ViewBag.VanDeID = 0;
-
+            ViewBag.ConnectionString = cnnString;
             return View();
-        }
-
-        public JsonResult LayDanhSachLoaiVanDe()
-        {
-            var result = IssueTypeModel.LoaiVanDe_LayDanhSach();       
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult CapNhatVanDe()
-        {            
-            IssueModel IM = new IssueModel();
-            IM.VanDeID = Int32.Parse(Request.Form["VanDeID"].ToString());
-            IM.DuAnID = Int32.Parse(Request.Form["DuAnID"].ToString());
-            IM.LoaiVanDeID = Int32.Parse(Request.Form["LoaiVanDe"].ToString());
-            IM.TenVanDe = Request.Form["TenVanDe"].ToString();
-            IM.MoTa = Request.Form["MoTa"].ToString();
-            IM.TrangThai = Int32.Parse(Request.Form["TrangThai"].ToString());
-            IM.NguoiThucHien = Request.Form["NguoiThucHien"].ToString();
-            IM.NgayBatDau = Request.Form["NgayBatDau"].ToString();
-            IM.NgayKetThuc = Request.Form["NgayKetThuc"].ToString();
-            IM.SoGioDuKien = Int32.Parse(Request.Form["SoGioDuKien"].ToString());
-            IM.SoGioThucTe = Int32.Parse(Request.Form["SoGioThucTe"].ToString());
-            IM.NguoiCapNhat = Session["UserName"].ToString();
-
-            ResData res = IssueModel.CapNhatVanDe(IM);
-            
-            return Json(res, JsonRequestBehavior.AllowGet);
         }
 
     }
