6cc927930c69d21bd0f93acc1e615c7ef2cec77e
VanHC <VanHC@VANHC>
2018-11-08 16:58:55 +0700

    add project Issue.List_New

---
+++ Issue/Issue.List_New/Areas/Issue/IssueAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Issue.List_New.Areas.Issue
+{
+    public class IssueAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Issue";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Issue_List",
+                "Issue/List_New/{action}/{id}",
+                new { Controller = "List", action = "List_New", id = UrlParameter.Optional }
+                , namespaces: new[] { "Issue.List_New.Areas.Issue.Controllers" }
+            );
+        }
+    }
+}
